89d86bd70cddedbf20480be744afb5e0ba7ced11
utkarshh0110 <utkarshh0110@gmail.com>
2021-04-28 20:09:10 +0530

    pagination implementation

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import jsonData from "./MOCK_DATA.json";
+import './App.css';
+import ReactPaginate from 'react-paginate';
+
+const App = () => {
+  const [users, setUsers] = useState(jsonData.slice(0, 500));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber*usersPerPage;
+
+  const displayUsers = users.slice(pagesVisited, pagesVisited + usersPerPage).map((user) => {
+    return (
+      <div className="user">
+        <h3>{user.id}</h3>
+        <h3>{user.first_name}</h3>
+        <h3>{user.last_name}</h3>
+        <h3>{user.email}</h3>
+      </div>
+    );
+  })
+
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected);
+  }
+
+  return (
+    <div className="App">
+      {displayUsers}
+      <ReactPaginate
+      previousLabel={"Previous"}
+      nextLabel={"Next"}
+      pageCount={pageCount}
+      onPageChange={changePage}
+      containerClassName={"paginationBttns"}
+      previousLinkClassName={"previousBttn"}
+      nextLinkClassName={"nextBttn"}
+      disabledClassName={"paginationDisabled"}
+      activeClassName={"paginationActive"}
+      />
+    </div>
+  );
+};
+
+export default App;
